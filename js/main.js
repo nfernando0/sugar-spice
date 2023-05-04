167d7abec5745bef0b0c3f2c354720b1236820d1
nfernando0 <nandof986@gmail.com>
2023-05-04 12:11:46 +0700

    menambah modal box

--- js/main.js
+++ js/main.js
@@ -1,31 +1,60 @@
 // Toggle Class active
-const navbarNav = document.querySelector('.navbar-nav');
+const navbarNav = document.querySelector(".navbar-nav");
+const searchBtn = document.querySelector("#search__btn");
+const shoppingCartBtn = document.querySelector("#shopping-cart-btn");
 
-document.querySelector('#hamb-menu').onclick = (e) => {
-    navbarNav.classList.toggle('active');
-    e.preventDefault();
-}
+document.querySelector("#hamb-menu").onclick = (e) => {
+  navbarNav.classList.toggle("active");
+  e.preventDefault();
+};
+
+document.querySelector("#shopping-cart-btn").onclick = (e) => {
+  e.preventDefault();
+  shoppingCart.classList.toggle("active");
+};
 
 // Search form
 
-const searchForm = document.querySelector('.search__form');
-const searchBox = document.querySelector('#search__box');
+const searchForm = document.querySelector(".search__form");
+const searchBox = document.querySelector("#search__box");
 
-document.querySelector('#search__btn').onclick = (e) => {
-    searchForm.classList.toggle('active');
-    searchBox.focus();
-    e.preventDefault();
-}
+document.querySelector("#search__btn").onclick = (e) => {
+  searchForm.classList.toggle("active");
+  searchBox.focus();
+  e.preventDefault();
+};
 
+const hamburger = document.querySelector("#hamb-menu");
+const shoppingCart = document.querySelector(".shopping-cart");
 
-const hamburger = document.querySelector('#hamb-menu');
-const searchBtn = document.querySelector('#search__btn');
+document.addEventListener("click", function (e) {
+  if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove("active");
+  }
+  if (!searchBtn.contains(e.target) && !searchForm.contains(e.target)) {
+    searchForm.classList.remove("active");
+  }
+  if (!shoppingCartBtn.contains(e.target) && !shoppingCart.contains(e.target)) {
+    shoppingCart.classList.remove("active");
+  }
+  if (!itemDetail.contains(e.target) && !itemModal.contains(e.target)) {
+    itemModal.classList.remove("active");
+  }
+});
 
-document.addEventListener('click', function(e) {
-    if(!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
-        navbarNav.classList.remove('active');
-    }    
-    if(!searchBtn.contains(e.target) && !searchForm.contains(e.target)) {
-        searchForm.classList.remove('active');
-    }    
-})
+// Modal Box
+
+const itemModal = document.querySelector("#item-detail-modal");
+const itemDetail = document.querySelector(".item-detail-btn");
+
+itemDetail.onclick = (e) => {
+  itemModal.style.display = "flex";
+  e.preventDefault();
+};
+
+const closeBtn = document.querySelector(".close-icon");
+
+closeBtn.onclick = (e) => {
+  itemModal.style.display = "none";
+  e.preventDefault();
+};
